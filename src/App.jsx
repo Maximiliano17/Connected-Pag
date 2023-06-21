0cd20685f0b31875b85269bd8d1750af5c6c8c5a
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-06-20 22:20:05 -0300

    Subir pagina

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//Pages
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <> 
+       <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+//Styles
+import styles from "../modules/Home.module.css"
+
+function Home() {
+    return (
+      <>
+          {/* Home */}
+      <main id="Home" className={styles.main}>
+        <div className={styles.content}>
+            <img />
+            <p>Connected World</p>
+            <p className={styles.slogan}>Por un mundo conectado, por un mundo mejor.</p> 
+            </div> 
+      </main>
+      </>
+    )
+  }
+  
+  export default Home
+  
