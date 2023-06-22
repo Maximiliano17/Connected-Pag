08db9b216839b866f3170533f5f1daae88ac5f9b
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-06-21 23:51:05 -0300

    Agregar secciones

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 //Styles
 import styles from "../modules/Home.module.css"
+//imagenes
+import img from "../assets/img/logo.png"
 
 function Home() {
     return (
       <>
           {/* Home */}
-      <main id="Home" className={styles.main}>
+      <main className={styles.main}>
         <div className={styles.content}>
-            <img />
-            <p>Connected World</p>
+            <img src={img} />
+            <h1>Connected World</h1>
             <p className={styles.slogan}>Por un mundo conectado, por un mundo mejor.</p> 
             </div> 
+      </main>
+      <main id="Home" className={styles.home}>
+        <h1>hola</h1>
       </main>
       </>
     )
