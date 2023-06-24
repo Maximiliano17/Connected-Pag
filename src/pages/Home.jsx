b6bea2c80c488720fcab66332c9622b9d4e6f8e2
Maximiliano17 <maxidossantosburgos@gmail.com>
2023-06-23 23:18:38 -0300

    Subir cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 //Styles
 import styles from "../modules/Home.module.css"
-//imagenes
-import img from "../assets/img/logo.png"
 
-function Home() {
-    return (
+function Home() { 
+    return ( 
       <>
           {/* Home */}
       <main className={styles.main}>
@@ -12,18 +10,23 @@ function Home() {
 
             </div> 
               <section className={styles.contenido}>
-                <section className={styles.img}>
-                 <img src={img} />          
-                </section>
-               <section className={styles.texto}>
-                <h1>Connected World</h1>
-                <p>Por un mundo conectado, por un mundo mejor.</p> 
-              </section>
+               
            </section>
       </main>
 
       <main id="Home" className={styles.home}>
-        <h1>hola</h1>
+       <section className={styles.tarjeta}>
+         <img />
+         <p>hola</p>
+       </section>
+       <section className={styles.tarjeta}>
+        <img />
+          <p>hola</p>
+        </section>
+        <section className={styles.tarjeta}>
+          <img />
+          <p>hola</p>
+        </section>
       </main>
       </>
     )
